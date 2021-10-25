55458a76a14abf8472fbb7b0e710610825c3be6a
StefanoConstantopulos <90971200+StefanoConstantopulos@users.noreply.github.com>
2021-10-25 23:07:20 +0200

    Add files via upload

---
+++ constantopulos.python.py
@@ -0,0 +1,34 @@
+x = 5
+y = 3
+
+print(x + y)
+
+x = 5
+y = 3
+
+print(x - y)
+
+x = 5
+y = 3
+
+print(x * y)
+
+x = 12
+y = 3
+
+print(x / y)
+
+x = 5
+y = 2
+
+print(x % y)
+
+x = 2
+y = 5
+
+print(x ** y)
+
+x = 15
+y = 2
+
+print(x // y)
