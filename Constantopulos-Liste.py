7063ffcd3fd4aaaa7b3f41e601d840bfbe82e562
StefanoConstantopulos <90971200+StefanoConstantopulos@users.noreply.github.com>
2021-11-16 23:29:09 +0100

    Add files via upload

---
+++ Constantopulos-Liste.py
@@ -0,0 +1,13 @@
+elencoUno = [ Casa, Pappa, Scuola, Coding, Python ]
+
+print( "elencoUno è una " , digita ( elencoUno ))
+
+lista = elencoUno
+
+print ( " \n l'elemento 1 di lista è " , lista [ 1 ] )
+
+print ( " \n la sua lunghezza è " , len ( lista ))
+
+lista  =  lista  + [ 'Casa' , 'Pappa' , 'Coding' ]
+
+print ( " \n la lista adesso è:" )
